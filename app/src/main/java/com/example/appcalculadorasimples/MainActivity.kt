df9e6bac792bb1d3ffe9df3a5869267ca4981750
Paulo César Conduru <133069658+pcconduru@users.noreply.github.com>
2025-02-02 21:13:14 -0300

    Atualização de Código 02-02-25

--- app/src/main/java/com/example/appcalculadorasimples/MainActivity.kt
+++ app/src/main/java/com/example/appcalculadorasimples/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.appcalculadorasimples
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.appcalculadorasimples.databinding.ActivityMainBinding
+import java.math.BigDecimal
+import java.math.BigInteger
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate( layoutInflater )
+    }
+    private lateinit var valorX : BigDecimal
+    private lateinit var valorY : BigDecimal
+    internal var estadoX : Boolean = false
+    internal var estadoY : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    
+
+    class operacoes{
+        fun somar(x: BigDecimal, y: BigDecimal) = { x + y }
+        fun subtrair(x: BigDecimal, y: BigDecimal) = { x - y }
     }
 }

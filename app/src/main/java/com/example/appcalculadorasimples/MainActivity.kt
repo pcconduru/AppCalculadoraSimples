7a085c9ef7ce2e264e4f4b1399fae90c79d04a08
Paulo César Conduru <133069658+pcconduru@users.noreply.github.com>
2025-02-03 19:47:07 -0300

    Atualização de Código - 03/02
    Criado mais botões para interface e teste do código da calculadora, usei uma abordagem diferente e refiz o código

--- app/src/main/java/com/example/appcalculadorasimples/MainActivity.kt
+++ app/src/main/java/com/example/appcalculadorasimples/MainActivity.kt
@@ -7,18 +7,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.appcalculadorasimples.databinding.ActivityMainBinding
-import java.math.BigDecimal
-import java.math.BigInteger
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy {
-        ActivityMainBinding.inflate( layoutInflater )
+        ActivityMainBinding.inflate(layoutInflater)
     }
-    private lateinit var valorX : BigDecimal
-    private lateinit var valorY : BigDecimal
-    internal var estadoX : Boolean = false
-    internal var estadoY : Boolean = false
+
+
+    private var operacaoAberta = StringBuilder()
+    private lateinit var ultimaOperacao : String
+    private lateinit var operador : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,11 +28,51 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-    
 
-    class operacoes{
-        fun somar(x: BigDecimal, y: BigDecimal) = { x + y }
-        fun subtrair(x: BigDecimal, y: BigDecimal) = { x - y }
+        var total : Int? = null
+
+        binding.btnClear.setOnClickListener {
+            operacaoAberta.clear()
+            ultimaOperacao = ""
+        }
+        binding.btnEquals.setOnClickListener {
+            when (operador){
+                "somar" -> total = 10 + operacaoAberta.toString().toInt()
+                "subtrair" -> total = ultimaOperacao.toInt() - operacaoAberta.toString().toInt()
+                "dividir" -> total = ultimaOperacao.toInt() * operacaoAberta.toString().toInt()
+                "multiplicar" -> total = ultimaOperacao.toInt() / operacaoAberta.toString().toInt()
+            }
+
+            binding.txtCalculo.setText(total.toString())
+        }
     }
+
+    fun operacaoClick(view: View){
+        var pegarOperacaoId = view.context.resources.getResourceEntryName(view.id)
+        ultimaOperacao = operacaoAberta.toString()
+        when ( pegarOperacaoId ){
+            "btnAdd" -> operador = "somar"
+            "btnDivider" -> operador = "dividir"
+            "btnSubtract" -> operador = "subtrair"
+            "btnTimes" -> operador = "multiplicar"
+        }
+    }
+
+    fun numeroClick(view: View){
+        var pegarNumeroId = view.context.resources.getResourceEntryName(view.id)
+        when ( pegarNumeroId ){
+            "btn0" -> operacaoAberta.append("0")
+            "btn1" -> operacaoAberta.append("1")
+            "btn2" -> operacaoAberta.append("2")
+            "btn3" -> operacaoAberta.append("3")
+            "btn4" -> operacaoAberta.append("4")
+            "btn5" -> operacaoAberta.append("5")
+            "btn6" -> operacaoAberta.append("6")
+            "btn7" -> operacaoAberta.append("7")
+            "btn8" -> operacaoAberta.append("8")
+            "btn9" -> operacaoAberta.append("9")
+        }
+        binding.txtCalculo.setText(operacaoAberta)
+    }
+
 }
